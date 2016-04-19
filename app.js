7963b4fbd45dd1526475c31cf3267716f7b533c7
PatrickSunico <apatricksunico@gmail.com>
2016-04-19 18:53:35 +0300

    Updated Date Formats + View Page Minor Changes

--- app.js
+++ app.js
@@ -4,12 +4,14 @@
 'use strict';
 
 var express = require('express'),
-app = express(),
-mongoose = require('mongoose'),
-bodyParser = require('body-parser'),
-moment = require('moment'),
-dateFormat = require('dateformat'),
-exphbs = require('express-handlebars');
+    app = express(),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
+    moment = require('moment'),
+    dateFormat = require('dateformat'),
+    Handlebars = require('handlebars'),
+    HandlebarsIntl = require('handlebars-intl'),
+    exphbs = require('express-handlebars');
 
 /*********************************************/
 
@@ -18,11 +20,18 @@ exphbs = require('express-handlebars');
 /*********************************************/
 
 //body parser
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 //view engine init
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+
+
 app.set('view engine', 'handlebars');
+HandlebarsIntl.registerWith(Handlebars);
 
 //Serve static assets
 app.use(express.static('public/'));
@@ -37,12 +46,19 @@ app.use(express.static('public/img/**.*'));
 
 mongoose.connect('mongodb://localhost/reposte_app');
 var blogSchema = new mongoose.Schema({
-  title: String,
-  name: String, //UserName
-  body: String,
-  created: {type: Date, default: Date.now}
+    title: String,
+    name: String, //UserName
+    body: String,
+    created: {
+        type: Date,
+        default: Date.now
+    }
 
-},{timestamps: {createdAt: 'created_at'}});
+}, {
+    timestamps: {
+        createdAt: 'created_at'
+    }
+});
 
 //Default Blog Model
 var Blog = mongoose.model('Blog', blogSchema);
@@ -64,8 +80,9 @@ var port = Number(process.env.PORT || 3000);
 
 //pages paths
 var paths = {
-  index: 'partials/index',
-  new: 'partials/newblog'
+    index: 'partials/index',
+    new: 'partials/newblog',
+    show: 'partials/show'
 };
 
 /*********************************************/
@@ -76,60 +93,66 @@ var paths = {
 /*********************************************/
 
 //Main index
-app.get('/', function(req,res){
-  res.redirect('/blogs');
+app.get('/', function(req, res) {
+    res.redirect('/blogs');
 });
 
 
 //Index show all blogs
-app.get('/blogs', function(req,res){
+app.get('/blogs', function(req, res) {
 
-  Blog.find({}, function(err, blogs){
-    var date;
-    blogs.forEach(function(index){
-      date = index.created.toDateString();
+    Blog.find({}, function(err, blogs) {
 
-      return date;
+        if (err) {
+            console.log(err);
+        } else {
+            res.render(paths.index, {
+                name: name,
+                blogs: blogs
+            });
+        }
     });
-
-
-    if(err){
-      console.log(err);
-    } else {
-      res.render(paths.index, {name:name, blogs:blogs, date:date});
-    }
-  });
 });
 
 //New Route
-app.get('/blogs/new', function(req, res){
-  res.render(paths.new,{name:name});
+app.get('/blogs/new', function(req, res) {
+    res.render(paths.new, {
+        name: name
+    });
 });
 
 //Create Route
 //POST
-app.post('/blogs', function(req, res){
+app.post('/blogs', function(req, res) {
 
-  Blog.create(req.body.blog, function(err, newBlog){
-    if(err){
-      res.render(paths.new);
-    } else {
-      res.redirect('/blogs');
-    }
-  });
+    Blog.create(req.body.blog, function(err, newBlog) {
+        if (err) {
+            res.render(paths.new);
+        } else {
+            res.redirect('/blogs');
+        }
+    });
 });
 
 // Show individual Blog route
 
-app.get('/blogs/:id', function(req, res){
-  res.send('show Page');
+app.get('/blogs/:id', function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if (err) {
+            console.log(err);
+            res.redirect('/blogs');
+        } else {
+            res.render(paths.show, {
+                blog: foundBlog
+            });
+        }
+    });
+
 });
 
 
-
-
 //listening port
-app.listen(port, function(){
-  console.log('Server Started');
+app.listen(port, function() {
+    console.log('Server Started');
 });
 /*********************************************/
